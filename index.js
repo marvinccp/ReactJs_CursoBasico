0f610604bbbcae52fef25c664ba8b039e43d01c9
[Marvin] <[marvinccp@gmail.com]>
2022-01-22 03:20:40 +0100

    Avance curso básico de React

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ class Component2 extends React.Component {
   render() {
     return (
       <div>
-        <h1>Class Component</h1>
+        <h1> Class Component </h1>
         <p>Parrafo</p>
       </div>
     );
