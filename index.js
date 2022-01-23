c23efc83084cbae96ba5a80f51fb676f0a5224a4
[Marvin] <[marvinccp@gmail.com]>
2022-01-24 00:26:24 +0100

    03. Props, children y eventos con React

--- index.js
+++ index.js
@@ -1,26 +1,64 @@
-const App = () => {
-  return (
-    <div>
-      <h1>Component</h1>
-      <Component2 />
-    </div>
-  );
-};
-
-class Component2 extends React.Component {
-  constructor() {
-    super();
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
   }
-
+  
   render() {
     return (
       <div>
-        <h1> Class Component </h1>
-        <p>Parrafo</p>
+        <h1>{this.props.name}</h1>
       </div>
     );
   }
 }
+
+const Main = ({ name2 }) =>{
+
+  
+  return <h1>{name2}</h1>
+}
+
+
+const Footer = ({ name3 }) =>{
+  return <h1>{name3}</h1>
+}
+
+const Button = ({ handleClick }) => {
+  return <button onClick={handleClick}>Check</button>;
+};
+
+const Page = ({children}) =>{
+  return (
+    <div> 
+      {children}
+    </div>
+  )
+}
+const App = () => {
+
+let name = 'Header'
+let name2 = 'Main'
+let name3 = 'Footer'
+
+const handleClick = () => {
+  alert("Learn React");
+};
+
+
+  return (
+    <div>
+      <Page>
+        <Header name={name} />
+
+        <Main name2={name2} />
+
+        <Footer name3={name3} />
+      </Page>
+
+      <Button handleClick={handleClick}/>
+    </div>
+  );
+};
 
 const root = document.getElementById("root");
 ReactDOM.render(<App />, root);
